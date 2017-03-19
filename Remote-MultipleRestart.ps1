0dab51469d50c26190b54b800b134474f31c1efb
rfduarte <ramon7.freitas@gmail.com>
2017-03-19 07:24:44 -0300

    Script creation

---
+++ Remote-MultipleRestart.ps1
@@ -0,0 +1,10 @@
+$Hosts = 'Computer1','Computer2','Computer3'
+$Username = "administrator"
+$Password = "password" | ConvertTo-SecureString -AsPlainText -Force
+$Credentials = New-Object System.Management.Automation.PSCredential($username,$password)
+
+ForEach-Object -InputObject $Hosts -Process {
+    Invoke-Command -ComputerName $Hosts -Credential $Credentials {Restart-Computer}
+}
+
+Write-Host -ForegroundColor "Green" Done!
